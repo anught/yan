773ecb2eee06e5a0e8e8b4996e83892d31d4affa
anught <782655752@qq.com>
2018-03-02 17:43:55 +0800

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -1,21 +1,14 @@
- 1 # -*- coding: utf-8 -*-
- 2 """
- 3 Created on Mon Mar 28 22:40:41 2016
- 4 
- 5 @author: zhanghc
- 6 """
- 7 
- 8 import socket
- 9 
-10 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-11 
-12 s.connect(('127.0.0.1',9999))
-13 
-14 print s.recv(1024)
-15 
-16 for data in ['zhang','liu','wang']:
-17     s.send(data)
-18     print s.recv(1024)
-19 
-20 s.send('exit')
-21 s.close()
+#!/usr/bin/env python                                                                                 
+import socket                                                                                         
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)                                                    
+s.connect(('127.0.0.1',8888))                                                                         
+                                                                                                    
+                                                                                                      
+for data in ['zhang','liu','wang']:                                                                   
+    s.send(data)                                                                                      
+    print s.recv(1024)                                                                                
+                                                                                                      
+s.send('exit')                                                                                        
+s.close()                                                                                             
+~                      
